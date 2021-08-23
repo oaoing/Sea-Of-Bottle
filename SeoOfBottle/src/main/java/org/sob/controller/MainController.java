473491aa0a60c03fa1d8abdecf7d4a9cc3519b67
shin-seoung-gyun <sg0220.shin@daum.net>
2021-08-23 17:49:58 +0900

    20210823 controller 테스트중

--- SeoOfBottle/src/main/java/org/sob/controller/MainController.java
+++ SeoOfBottle/src/main/java/org/sob/controller/MainController.java
@@ -8,6 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j;
@@ -21,46 +22,50 @@ public class MainController {
 	
 	private MainService service;
 	
-	//목록
+	//목록 전제척으로 rttr기능 하는지 테스트가 필요함.(객체 히든 저장이 필요한가)
 	@GetMapping("/main")//자기자신.jsp 페이지 오픈
-	public void list(UserVO uvo, Model model) {
+	public void list(UserVO uvo, Model model) {//, String userid 테스트용
 		log.info("목록페이지요청");
+//		int user = Integer.parseInt(userid);//테스트용
+//		model.addAttribute("list",service.getList(user));
+//		model.addAttribute("UserVO",uvo);
 		model.addAttribute("list",service.getList(uvo.getUserId()));
 		model.addAttribute("UserVO",uvo);
 		
 	}
 	
 	//등록
-	@PostMapping("/register")//새유리병, 이어쓴 편지 등록
-	public String register(UserVO uvo, MainVO mvo) {
+	@PostMapping("/register")//새유리병, 이어쓴 편지 등록 확인 필요 (돌아가는지)
+	public String register(UserVO uvo,RedirectAttributes rttr, MainVO mvo) {
 		//편지 등록 후 list페이지로 돌아감
 		
 		service.register(mvo); 
-		
-		return "redirect:/sob/main";//리다이렉트로 보냄//get형식으로 url에 값 실어서 보내기 가능
+		rttr.addFlashAttribute("uvo",uvo);
+		return "redirect:/sob/main?";//리다이렉트로 보냄//get형식으로 url에 값 실어서 보내기 가능
 	}	
 	
 	@GetMapping("/register")//등록페이지 jsp 열기 groupid 추후 수정
-	public void register() {
-		
+	public void register(Model model,UserVO uvo) {
+		model.addAttribute("UserVO",uvo);
 	}
 	
 	//상세
 	@GetMapping("/get")
-	public void get(String groupId, Model model) {
+	public void get(String groupId, Model model,UserVO uvo) {
 		log.info("유리병 보기 요청");
 		model.addAttribute("latter",service.get(groupId));
 		model.addAttribute("groupId",groupId);
-			
+		model.addAttribute("UserVO",uvo);
+		
 	}
 	
 	//버리기
 	@PostMapping("/remove")
-	public String remove(String groupId) {
+	public String remove(String groupId,UserVO uvo,RedirectAttributes rttr) {
 		log.info("유리병버리기 요청");
 		service.remove(groupId);
-		
-		return "redirect:/sob/main";
+		rttr.addFlashAttribute("uvo",uvo);
+		return "redirect:/sob/main?";
 	}
 	
 	
---
+++ SeoOfBottle/src/test/java/org/sob/controller/MainControllerTest.java
@@ -0,0 +1,53 @@
+package org.sob.controller;
+
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.sob.domain.UserVO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import lombok.extern.log4j.Log4j;
+
+@Log4j
+@RunWith(SpringJUnit4ClassRunner.class)//j유닛 사용할때 스프링을 사용하겠다.
+@ContextConfiguration({"file:src/main/webapp/WEB-INF/spring/root-context.xml",
+	"file:src/main/webapp/WEB-INF/spring/appServlet/servlet-context.xml"})//서버사용 테스트는 이걸로
+@WebAppConfiguration
+public class MainControllerTest {
+	@Autowired
+	private WebApplicationContext ctx;
+	
+	private MockMvc mockMvc;
+	
+	@Before
+	public void setup() {
+		mockMvc = MockMvcBuilders.webAppContextSetup(ctx).build();
+	}
+	
+	@Test
+	public void testList() throws Exception{//test ok
+		
+		log.info(mockMvc.perform(MockMvcRequestBuilders.get("/sob/main")
+		.param("userid","1"))
+		.andReturn()
+		.getModelAndView()
+		.getModelMap());
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
